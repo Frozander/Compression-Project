26b4452e997ce0289264dbdeca9e063e967c65b4
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2021-06-04 18:02:27 +0300

    Final final lulw

--- test.py
+++ test.py
@@ -19,14 +19,15 @@ def main():
             compressor.write_to_file('lossless.froz')
             # compressor.write_to_file('lossless_corrupt.froz')
 
-            # CORRUPT FILE
+            # CORRUPT FILE (This part can be used to corrupt files)
             # f = open("lossless_corrupt.froz", "rb+")
             # f.seek(0)
-            # f.write(b'123456')
+            # f.write(b'1234567890')
             # f.close()
 
             # compressor.read_file('lossless_corrupt.froz')
             # compressor.write_to_file("out_corrupt.txt")
+
             compressor = FrozCompressor()
             compressor.read_file('lossless.froz')
             compressor.write_to_file("out.txt")
@@ -47,17 +48,18 @@ def main():
             print(f"Compressed Data Size (bytes): {compressed}")
             print(f"Compression Ratio: {size / compressed}")
             print(f"Compression Efficiency: {efficiency}%")
-            print()
 
             # Compare outputs
-            # with open(test_tiny, "r") as f:
-            #     original_data = f.read().strip().splitlines()
-            # with open("out_corrupt.txt", "r") as f:
-            #     decoded_data = f.read().strip().splitlines()
+            with open(file, "r") as f:
+                original_data = f.read().strip().splitlines()
+            with open("out.txt", "r") as f:
+                decoded_data = f.read().strip().splitlines()
 
-            # print("Diff between original data and decoded data:")
-            # for line in difflib.unified_diff(original_data, decoded_data, fromfile=file_name, tofile='out.txt', lineterm='', n=0):
-            #     print(line)
+            print("Diff between original data and decoded data:")
+            for line in difflib.unified_diff(original_data, decoded_data, fromfile=file, tofile='out.txt', lineterm='', n=0):
+                print(line)
+
+            print()
 
 
 if __name__ == "__main__":
